38254184904bd301e9161069beb76918a65c41d3
RuhanVhora <ruhanvhora72@gmail.com>
2024-11-18 19:03:26 -0700

    1st commit

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const port = 3000 || 8000;
-
+ 
+app.use((req,res,next)=>{
+    console.log(`Request recevied: ${req.method}, ${req.url}`);
+    next();
+});
 app.get("/user/:id",(req,res) => {
-    const userId = req.params.id;//dot notation
-    res.send('user id is ${userId}');
+    const userId = req.params.id ; // dot notation
+    res.send(`User id is ${userId}`);
 });
 
+app.get("/search",(req,res) => {
+    const query =req.params.q;
+    res.send (`Search query is : ${query}`);
+});
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => {
-    console.log('server started on ${port}');
-});
+    console.log(`Connected to port ${port}`)
+})
